c4615ff9c1a01245dcd481d5105ac287fce73339
Pooja Lodhi <lodhipooja952@gmail.com>
2026-08-30 13:56:42 +0530

    updated linked list code

--- listNodeClass.java
+++ listNodeClass.java
@@ -1,14 +1,35 @@
-class Node{
+class Node {
+
     int val;
     Node next;
-    Node(int val){
-        this.val=val;
+
+    Node(int val) {
+        this.val = val;
     }
 }
-public class listNodeClass{
-    public static void main(String [] args){
+
+public class listNodeClass {
+    public static void print(Node head){
+        Node temp = head;
+        while(temp != null){
+            System.out.println(temp.val);
+            temp = temp.next;
+        }
+    }
+    public static void displayRecursive(Node head){
+        if(head==null){
+            return;
+        }
+        System.out.println(head.val);
+        displayRecursive(head.next);
+    }
+
+    public static void main(String[] args) {
+
         Node a = new Node(10);
+
         Node b = new Node(20);
-        a.next=b;
+
+        a.next = b;
     }
 }
